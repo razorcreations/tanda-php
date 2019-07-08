334444a2f0d490f3af98442dfa24b5f7b80d45a9
Ross Cooper <ross@razorcreations.com>
2019-07-08 13:36:30 +0100

    Added new Resource and User class

---
+++ src/Resource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RazorCreations\Tanda;
+
+abstract class Resource {
+
+	const DATEFORMAT = 'YYYY-MM-DD';
+	
+	protected $dates = [
+		'created_at',
+		'updated_at',
+	];
+
+	public function __construct(array $args)
+	{
+		foreach (get_object_vars($this) as $key => $value) {
+			if (isset($args[$key])) {
+				if (isset($this->dates[$key])) {
+					$this->key = date(self::DATEFORMAT, $args[$key]);
+					continue;
+				}
+
+				$this->$key = $args[$key];
+			}
+		}
+	}
+	
+	public static function fromArray(array $args)
+	{
+		return new static($args);
+	}
+}
---
+++ src/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RazorCreations\Tanda;
+
+class User extends Resource {
+
+	public $id;
+	public $name;
+	public $date_of_birth;
+	public $employment_start_date;
+	public $employee_id;
+	public $passcode;
+	public $user_levels;
+	public $department_ids;
+	public $prefered_hours;
+	public $active;
+	public $enable_login;
+	public $email;
+	public $photo;
+	public $phone;
+	public $normalised_phone;
+	public $time_zone;
+	public $created_at;
+	public $updated_at;
+}
